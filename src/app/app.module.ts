9eecbe77e9a074cda9d6cc72f0b8df5d76c13fa9
Cherbadzhy <CHERBY@de.softlab.net>
2018-11-15 13:38:44 +0100

    generated structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ModelsComponent } from './models/models.component';
+import { OverviewComponent } from './overview/overview.component';
+import { ProjectComponent } from './project/project.component';
+import { TimeEntryComponent } from './time-entry/time-entry.component';
+import { EntryEditComponent } from './time-entry/entry-edit/entry-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ModelsComponent,
+    TimeEntryComponent,
+    OverviewComponent,
+    ProjectComponent,
+    EntryEditComponent
   ],
   imports: [
     BrowserModule
