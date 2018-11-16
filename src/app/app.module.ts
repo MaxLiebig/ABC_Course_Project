d3d7f1b23976e198392c10d60391934fac99b8de
jcherbadzhy <yulkachirba@gmail.com>
2018-11-16 13:31:20 +0100

    merged app-module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,7 @@ import { UserService } from './service/user.service';
 import { TimeEntryService } from './service/time-entry.service';
 import { AppRoutingModule } from './app-routing.module';
 import { ProjectEditComponent } from './project/project-edit/project-edit.component';
+import { UserService } from './service/user.service';
 import { ProjectService } from './service/project.service';
 import { EntryItemComponent } from './time-entry/entry-item/entry-item.component';
 import { EntryListComponent } from './time-entry/entry-list/entry-list.component';
@@ -41,7 +42,7 @@ import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
     HttpModule,
     NgbModule
   ],
-  providers: [ UserService, ProjectService, TimeEntryService, FormatterService],
+  providers: [ UserService, TimeEntryService, FormatterService, ProjectService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
